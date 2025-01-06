914ddcb873ac8937077ea55aa1e574af2db00de1
Gustavo <gustavo.h.a.cavalcanti@gmail.com>
2025-01-06 19:23:31 -0300

    documentação com doxygen

--- conta_palavras.hpp
+++ conta_palavras.hpp
@@ -1,10 +1,34 @@
+// Copyright 2025 Gustavo
 #ifndef CONTA_PALAVRAS_HPP
 #define CONTA_PALAVRAS_HPP
 
 #include <map>
 #include <string>
 
-// Declaração da função que conta as palavras
+/**
+ * @file conta_palavras.hpp
+ * 
+ * @brief Declaração da função para contar as palavras em um texto.
+ * 
+ * Este arquivo contém a declaração da função `contarPalavras`, que é 
+ * responsável por contar a frequência de ocorrência de cada palavra em um 
+ * texto. Ele utiliza um mapa para armazenar as palavras e suas respectivas 
+ * contagens.
+ */
+
+/**
+ * @brief Conta a ocorrência de palavras em um texto.
+ * 
+ * A função `contarPalavras` recebe uma string contendo um texto e retorna 
+ * um mapa onde as chaves são as palavras do texto e os valores são o número 
+ * de vezes que cada palavra aparece. Caracteres não alfanuméricos são 
+ * removidos das palavras e a contagem é feita de forma insensível a maiúsculas 
+ * e minúsculas.
+ * 
+ * @param texto A string que contém o texto a ser analisado.
+ * 
+ * @return Um mapa de palavras (chaves) e suas contagens (valores).
+ */
 std::map<std::string, int> contarPalavras(const std::string& texto);
 
 #endif
